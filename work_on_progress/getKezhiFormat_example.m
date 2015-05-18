cac2f8448673c33365ea81441e7f57c772491f0b
ver228 <ver228@gmail.com>
2015-05-18 14:59:50 +0100

    keg format example

--- work_on_progress/getKezhiFormat_example.m
+++ work_on_progress/getKezhiFormat_example.m
@@ -1,5 +1,19 @@
-filename = '/Users/ajaver/Desktop/Gecko_compressed/20150511/kezhi_format/Capture_Ch1_11052015_195105/worm_2502.hdf5';
+movies_file = '/Volumes/behavgenom$/GeckoVideo/Invidual_videos/20150512/Capture_Ch1_12052015_194303/';
+kezhi_file = '/Volumes/behavgenom$/GeckoVideo/kezhi_format/20150512/Capture_Ch1_12052015_194303/';
+
+
+filename = [kezhi_file, 'worm_12.hdf5'];
+
+
 
 masks = h5read(filename, '/masks');
 frames = h5read(filename, '/frames');
 CMs = h5read(filename, '/CMs');
+
+%plot movement of the center of mass
+figure
+plot(CMs(1,:), CMs(2,:))
+
+%select a frame
+frame = 10;
+imshow(masks(:,:,10),[])
