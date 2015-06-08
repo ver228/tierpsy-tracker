678b7990da87bc792b7f7917e5973c6956e7c3b9
ver228 <ver228@gmail.com>
2015-06-08 19:54:27 +0100

    .

--- installation_script.sh
+++ installation_script.sh
@@ -17,13 +17,15 @@ cmake "Unix Makefile" -D BUILD_opencv_python3=ON \
 -D PYTHON3_INCLUDE_DIR=/usr/local/Cellar/python3/3.4.3/Frameworks/Python.framework/Versions/3.4/include/python3.4m/ \
 -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.4.3/Frameworks/Python.framework/Versions/3.4/lib/libpython3.4m.dylib \
 -D PYTHON3_PACKAGES_PATH=/usr/local/lib/python3.4/site-packages \
--D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/local/lib/python3.4/site-packages/numpy/core/include .
-
+-D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/local/lib/python3.4/site-packages/numpy/core/include \
+-D FFMPEG_INCLUDE_DIR=/usr/local/include \
+-D FFMPEG_LIB_DIR=/usr/local/include/../lib \
+.
 make -j24
 make install
 
 cd ..
-rm -p opencv-3.0.0
+rm -rf opencv-3.0.0
 rm 3.0.0.zip
 
 brew install --upgrade pyqt --with-python3
