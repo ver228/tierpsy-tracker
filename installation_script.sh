0b85f0b7b43166b95a14516e382696fb5c40adc8
ver228 <ver228@gmail.com>
2015-10-13 12:39:26 +0100

    .

--- installation_script.sh
+++ installation_script.sh
@@ -1,11 +1,25 @@
-sudo echo "Thanks."
-
 MW_MAIN_DIR=`pwd`
 
+#get sudo permissions
+sudo echo "Thanks."
+#make sure the /usr/loca/ is writable by the user
 sudo chown -R `whoami` /usr/local/
 
+#old ffmpeg to read mjpg
+cd $MW_MAIN_DIR
+rm -rf $MW_MAIN_DIR/opencv-3.0.0
+rm $MW_MAIN_DIR/opencv-3.0.0.zip
 
+curl http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_27.03.2014.zip > $MW_MAIN_DIR/ffmpeg_old.zip
+unzip $MW_MAIN_DIR/ffmpeg_old.zip ffmpeg
+sudo mv ffmpeg /usr/local/bin/ffmpeg22
+rm $MW_MAIN_DIR/ffmpeg_old.zip
+
+#install homebrew and other software used
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install cmake
+brew install ffmpeg
 
 #python dependencies
 brew install python3
@@ -30,10 +44,7 @@ brew uninstall --force pyqt5
 brew install pyqt5 --with-python3
 
 #install opencv
-brew install cmake
-brew install ffmpeg
-
-curl https://github.com/Itseez/opencv/archive/3.0.0.zip > $MW_MAIN_DIR/opencv-3.0.0.zip
+wget https://github.com/Itseez/opencv/archive/3.0.0.zip -O $MW_MAIN_DIR/opencv-3.0.0.zip
 unzip $MW_MAIN_DIR/opencv-3.0.0.zip
 cd $MW_MAIN_DIR/opencv-3.0.0
 
@@ -41,10 +52,10 @@ PY_VER=`python3 -c "import sys; print(sys.version.partition(' ')[0])"`
 PY_VER_SHORT=`python3 -c "import sys; print('.'.join(sys.version.partition(' ')[0].split('.')[0:2]))"`
 cmake '"Unix Makefile"' -DBUILD_opencv_python3=ON \
 -DBUILD_opencv_python2=OFF \
--DPYTHON3_INCLUDE_DIR=/usr/local/Cellar/python3/$PY_VER/Frameworks/Python.framework/Versions/$PY_VER_SHORT/include/python$PY_VER_SHORTm/ \
--DPYTHON3_LIBRARY=/usr/local/Cellar/python3/$PY_VER/Frameworks/Python.framework/Versions/$PY_VER_SHORT/lib/libpython$PY_VER_SHORTm.dylib \
--DPYTHON3_PACKAGES_PATH=/usr/local/lib/python$PY_VER_SHORT/site-packages \
--DPYTHON3_NUMPY_INCLUDE_DIRS=/usr/local/lib/python$PY_VER_SHORT/site-packages/numpy/core/include \
+-DPYTHON3_INCLUDE_DIR=/usr/local/Cellar/python3/${PY_VER}/Frameworks/Python.framework/Versions/${PY_VER_SHORT}/include/python${PY_VER_SHORT}m/ \
+-DPYTHON3_LIBRARY=/usr/local/Cellar/python3/${PY_VER}/Frameworks/Python.framework/Versions/${PY_VER_SHORT}/lib/libpython${PY_VER_SHORT}m.dylib \
+-DPYTHON3_PACKAGES_PATH=/usr/local/lib/python${PY_VER_SHORT}/site-packages \
+-DPYTHON3_NUMPY_INCLUDE_DIRS=/usr/local/lib/python${PY_VER_SHORT}/site-packages/numpy/core/include \
 -DBUILD_TIFF=ON -DBUILD_opencv_java=OFF -DWITH_CUDA=OFF -DENABLE_AVX=ON -DWITH_OPENGL=ON -DWITH_OPENCL=ON \
 -DWITH_IPP=ON -DWITH_TBB=ON -DWITH_EIGEN=ON -DWITH_V4L=ON -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF \
 -DWITH_QT=ON -DINSTALL_PYTHON_EXAMPLES=ON \
@@ -53,15 +64,6 @@ cmake '"Unix Makefile"' -DBUILD_opencv_python3=ON \
 make -j24
 make install
 
-#old ffmpeg to read mjpg
-cd $MW_MAIN_DIR
-rm -rf $MW_MAIN_DIR/opencv-3.0.0
-rm $MW_MAIN_DIR/opencv-3.0.0.zip
-
-curl http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_27.03.2014.zip > $MW_MAIN_DIR/ffmpeg_old.zip
-unzip $MW_MAIN_DIR/ffmpeg_old.zip ffmpeg
-sudo mv ffmpeg /usr/local/bin/ffmpeg22
-rm $MW_MAIN_DIR/ffmpeg_old.zip
 
 #compile cython files
 cd $MW_MAIN_DIR/MWTracker/trackWorms/segWormPython/cythonFiles/
