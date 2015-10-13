c263ae9862d55384bdf39e64678bb37e4540cb78
ver228 <ver228@gmail.com>
2015-10-13 11:48:11 +0100

    .

--- installation_script.sh
+++ installation_script.sh
@@ -4,22 +4,34 @@ MW_MAIN_DIR=`pwd`
 
 sudo chown -R `whoami` /usr/local/
 
-curl http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_27.03.2014.zip > $MW_MAIN_DIR/ffmpeg_old.zip
-unzip $MW_MAIN_DIR/ffmpeg_old.zip ffmpeg
-sudo mv ffmpeg /usr/local/bin/ffmpeg22
-rm $MW_MAIN_DIR/ffmpeg_old.zip
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install ffmpeg
+
+#python dependencies
 brew install python3
 brew install homebrew/science/hdf5
-brew install cmake
-brew install numpy --with-python3
+
+pip3 install numpy
+pip3 install spyder
+pip3 install tables
+pip3 install pandas
+pip3 install h5py
+pip3 install matplotlib
+pip3 install scipy
+pip3 install scikit-learn
+pip3 install scikit-image
+pip3 install tifffile
+pip3 install seaborn
+
+#install pyqt5 for the GUI
 brew uninstall --force sip
 brew install sip --with-python3
 brew uninstall --force pyqt5
 brew install pyqt5 --with-python3
 
+#install opencv
+brew install cmake
+brew install ffmpeg
 
 curl https://github.com/Itseez/opencv/archive/3.0.0.zip > $MW_MAIN_DIR/opencv-3.0.0.zip
 unzip $MW_MAIN_DIR/opencv-3.0.0.zip
@@ -41,22 +53,17 @@ cmake '"Unix Makefile"' -DBUILD_opencv_python3=ON \
 make -j24
 make install
 
+#old ffmpeg to read mjpg
 cd $MW_MAIN_DIR
 rm -rf $MW_MAIN_DIR/opencv-3.0.0
 rm $MW_MAIN_DIR/opencv-3.0.0.zip
 
-pip3 install spyder
-pip3 install tables
-pip3 install pandas
-pip3 install h5py
-pip3 install matplotlib
-pip3 install scipy
-pip3 install scikit-learn
-pip3 install scikit-image
-pip3 install tifffile
-pip3 install seaborn
+curl http://ffmpegmac.net/resources/SnowLeopard_Lion_Mountain_Lion_Mavericks_27.03.2014.zip > $MW_MAIN_DIR/ffmpeg_old.zip
+unzip $MW_MAIN_DIR/ffmpeg_old.zip ffmpeg
+sudo mv ffmpeg /usr/local/bin/ffmpeg22
+rm $MW_MAIN_DIR/ffmpeg_old.zip
 
-
+#compile cython files
 cd $MW_MAIN_DIR/MWTracker/trackWorms/segWormPython/cythonFiles/
 make
 make clean
