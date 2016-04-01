fa137801d018960b6ad75349bf7e23c81d76f3ac
ver228 <ver228@gmail.com>
2016-04-01 17:24:05 +0100

    .

--- MWTracker_GUI/MWTrackerViewer.py
+++ MWTracker_GUI/MWTrackerViewer.py
@@ -1,16 +1,12 @@
 import os, sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + os.sep + '..')
-
 from MWTracker.GUI.MWTrackerViewer.MWTrackerViewer_GUI import MWTrackerViewer_GUI
-#from PyQt5.QtWidgets import QApplication
-from PyQt4.QtGui import QApplication
+from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	
-	
-	ui = MWTrackerViewer_GUI(sys.argv)
-	ui.show()
-	app.exec_()
-	#sys.exit()
+    app = QApplication(sys.argv)
+    
+    ui = MWTrackerViewer_GUI()
+    ui.show()
+    
+    sys.exit(app.exec_())
--- single_worm_db/track_mac1.sh
+++ single_worm_db/track_mac1.sh
@@ -1,13 +1,13 @@
 python3 ~/Documents/GitHub/Multiworm_Tracking/cmd_scripts/trackMultipleFiles.py \
 /Volumes/behavgenom_archive\$/MaskedVideos \
 --json_file ~/Documents/GitHub/Multiworm_Tracking/JSON_singleworm/swimming.json \
---is_single_worm --max_num_process 21 --end_point INT_SKE_ORIENT\
+--is_single_worm --max_num_process 21 --end_point INT_SKE_ORIENT \
 --videos_list ~/Documents/GitHub/Multiworm_Tracking/single_worm_db/masks_swimming_1
 
 python3 ~/Documents/GitHub/Multiworm_Tracking/cmd_scripts/trackMultipleFiles.py \
 /Volumes/behavgenom_archive\$/MaskedVideos \
 --json_file ~/Documents/GitHub/Multiworm_Tracking/JSON_singleworm/on_food.json \
---is_single_worm --max_num_process 21 --end_point INT_SKE_ORIENT\
+--is_single_worm --max_num_process 21 --end_point INT_SKE_ORIENT \
 --videos_list ~/Documents/GitHub/Multiworm_Tracking/single_worm_db/masks_agar_1
 
 
