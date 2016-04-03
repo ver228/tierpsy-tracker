2e3b232f7415ba9c6f3539dee1a8b1c9227a0b21
ver228 <ver228@gmail.com>
2016-04-03 15:05:31 +0100

    pyinstaller tests

--- work_in_progress/test_pyinstaller/dum.py
+++ work_in_progress/test_pyinstaller/dum.py
@@ -1,11 +1,41 @@
 import numpy as np
-import cv2
 import sys
+
+#import matplotlib
+#matplotlib.use("Qt5Agg")
+#import matplotlib.pylab as plt
+import pandas as pd
+import cv2
+import h5py
 import tables
 
-a=np.arange(10)
-print(a)
-print(cv2.__version__)
-inputFiles="test.h5"
-with tables.File(inputFiles, 'w') as inputFileOpen:
-	pass
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    print('HOLA')
+    a = np.arange(10)
+    print(a)
+       
+
+    print(cv2.__version__)
+
+    inputFiles = "test.h5"
+    with h5py.File(inputFiles, 'w') as inputFileOpen:
+        print('good h5py')
+    
+    with tables.File(inputFiles, 'w') as inputFileOpen:
+        print('good tables')
+    
+    sys.exit(app.exec_()) 
+    
+    
---
+++ work_in_progress/test_pyinstaller/good_command.sh
@@ -0,0 +1,3 @@
+pyinstaller MWTrackerViewer.py --exclude-module PyQt4 --exclude-module PyQt4.QtCore --exclude-module PyQt4.QtGui \
+--hidden-import=h5py.defs --hidden-import=h5py.utils --hidden-import=h5py.h5ac --hidden-import='h5py._proxy' \
+-y --clean --onefile --windowed
