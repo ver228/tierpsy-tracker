94e5f6c226b0fb2d2acd9f98f4248ad80b7dd97d
ver228 <ver228@gmail.com>
2016-04-19 15:49:11 +0100

    bug

--- single_worm_db/files2analyze/video2maskDir.sh
+++ single_worm_db/files2analyze/video2maskDir.sh
@@ -1,4 +1,4 @@
 for NAME in `ls videos*.txt`
 do
-	sed -e s/thecus/MaskedVideos/g $NAME > ${NAME/videos/masks}
+	sed -e s/thecus/MaskedVideos/g $NAME | sed s/.avi/.hdf5/g > ${NAME/videos/masks}
 done
