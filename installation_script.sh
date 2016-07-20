a6b96cf5b780ee6b872e7d6180d50b945fb0d1f2
ver228 <ver228@gmail.com>
2016-07-20 01:32:02 +0100

    .

--- installation_script.sh
+++ installation_script.sh
@@ -188,13 +188,13 @@ OS=$(uname -s)
 case "${OS}" in
 	"Darwin")
 	#install_dependencies_osx
-	MINICONDA_LINK="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+	MINICONDA_LINK="https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
 	BASH_PROFILE_FILE=$HOME/.bash_profile
 	;;
 	
 	"Linux"*)
 	install_dependencies_linux
-	MINICONDA_LINK="https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+	MINICONDA_LINK="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
 	BASH_PROFILE_FILE=$HOME/.bashrc
 	;;
 esac
