2c86b53f42ec9f5abecccddb8354b33ba3fea9ab
ver228 <ver228@gmail.com>
2016-07-28 13:42:45 +0100

    reorganize
    - Rearrange directories
    - Remove tiff capture

--- _old/compress_cmd.sh
+++
@@ -1,5 +0,0 @@
-python compressParallel.py -i "/Volumes/G/20150416/" -o "/Volumes/behavgenom$/GeckoVideo/Compressed/20150416/" > progress.txt
-python trackParallelProcesses.py 20150416 > progress-1.txt
-python trackParallelProcesses.py 20150403 > progress-2.txt
-
-
