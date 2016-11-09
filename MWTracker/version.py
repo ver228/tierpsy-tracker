c03f4090031d9f701563281c340a8ff86091eafb
ver228 <ajaver@MRC-8791.local>
2016-11-09 15:03:54 +0000

    .

--- MWTracker/version.py
+++ MWTracker/version.py
@@ -2,6 +2,9 @@
 __version__ = '1.1.0'
 
 '''
+ 1.1.1
+ - Cumulative changes and bug corrections.
+
  1.1.0 
  - Fish tracking and fluorescence tracking.
  - major changes in the internal paralization, should make easier to add or remove steps on the tracking.
