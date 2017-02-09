b6acbad54d6bc14c262e86c8e22fb5a091fccf96
ver228 <ajaver@MRC-8791.local>
2017-02-09 16:19:06 +0000

    update

--- MATLAB_examples/read_mask.m
+++ MATLAB_examples/read_mask.m
@@ -1,22 +1,12 @@
-filename = '/Volumes/behavgenom$/GeckoVideo/Compressed/20150216/CaptureTest_90pc_Ch1_16022015_174636.hdf5';
-if ~exist('infoFile', 'var')
-    infoFile =  h5info(filename);
-end
+%filename = '/Volumes/behavgenom$/GeckoVideo/Compressed/20150216/CaptureTest_90pc_Ch1_16022015_174636.hdf5';
+filename = '/Volumes/behavgenom_archive$/Serena/CurrentDatasetSpiking/MaskedVideos/recording52/recording52.1g100-350TIFF/recording52.1g_X1.hdf5';
 
-chunkSize = [2048 2048 1];
+%get the chunksize
+mask_info =  h5info(filename, '/mask');
+chunkSize = mask_info.ChunkSize; %image size in my format
 
-image = h5read(filename, '/mask', [1,1,1], chunkSize);
-[~, rect] = imcrop(image);
-
+%%
 for frame_number = 1:25:200
-    image = h5read(filename, '/mask', [1,1,frame_number], chunkSize);
-    figure, imshow(imcrop(image, rect))
+    image = h5read(filename, '/mask', [1,1,frame_number], chunkSize)';
+    figure, imshow(image)
 end
-
-
-timestamp_time = h5read(mask_file, '/timestamp/time');
-timestamp_raw = h5read(mask_file, '/timestamp/raw');
-
-stage_data = h5read(mask_file, '/stage_data');
-h5read(mask_file, '/xml_info')
-%imshow(image)
