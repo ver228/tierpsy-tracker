738530471037136110711d56c48415c68d291ab5
ver228 <ajaver@MRC-8791.local>
2017-06-01 19:16:32 +0100

    script to correct wrong expected_fps

---
+++ tierpsy/helper/add_default_attrs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  8 16:19:07 2017
+
+@author: ajaver
+"""
+
+import glob
+import os
+import tables
+
+from tierpsy.helper.misc import RESERVED_EXT
+from tierpsy.helper.params import set_unit_conversions, read_unit_conversions
+from tierpsy import DFLT_PARAMS_PATH, DFLT_PARAMS_FILES
+from tierpsy.helper.params import TrackerParams
+
+#script to correct a previous bug in how the expected_fps, microns_per_pixel are saved.
+
+params = TrackerParams(os.path.join(DFLT_PARAMS_PATH, 'filter_worms.json'))
+expected_fps = params.compress_vid_param['expected_fps']
+microns_per_pixel = params.compress_vid_param['microns_per_pixel']
+
+main_dir = '/Volumes/behavgenom_archive$/Adam/screening'
+fnames = glob.glob(os.path.join(main_dir, '**', '*.hdf5'), recursive=True)
+
+masked_files = [x for x in fnames if not any(x.endswith(ext) for ext in RESERVED_EXT)]
+skeletons_files = [x for x in fnames if x.endswith('_skeletons.hdf5')]
+
+
+def change_attrs(fname, field_name):
+    print(os.path.basename(fname))
+    read_unit_conversions(fname)
+    with tables.File(fname, 'r+') as fid:
+        group_to_save = fid.get_node(field_name)
+        set_unit_conversions(group_to_save, 
+                             expected_fps=expected_fps, 
+                             microns_per_pixel=microns_per_pixel)
+        
+    read_unit_conversions(fname)
+
+#for mask_file in masked_files:
+#    change_attrs(mask_file, '/mask')
+
+
+for skeletons_file in skeletons_files:
+    change_attrs(skeletons_file, '/trajectories_data')
+ 
