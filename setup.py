038d6fd45f66274cc2f73b83d33e5832060fbbcc
ver228 <ajaver@MRC-8791.local>
2017-07-05 17:46:48 +0100

    improve installation

--- setup.py
+++ setup.py
@@ -7,45 +7,54 @@ from Cython.Distutils import build_ext
 import numpy
 import os
 
-
-module_name = 'tierpsy'
+NAME = "tierpsy"
+AUTHOR = 'Avelino Javer'
+AUTHOR_EMAIL = 'avelino.javer@imperial.ac.uk'
+URL = 'https://github.com/ver228/tierpsy-tracker'
+DOWNLOAD_URL = 'https://github.com/ver228/tierpsy-tracker'
+DESCRIPTION = "tierpsy: Tierpsy Tracker Multi-Worm Tracker."
 exec(open(module_name + '/version.py').read())
+VERSION = __version__
 
-#build cython files
-# python3 setup.py build_ext --inplace
-path_parts = [module_name, 'analysis', 'ske_create', 'segWormPython', 'cython_files']
-cython_path = os.path.join(*path_parts)
-cython_path_e = os.path.join(module_name, 'analysis', 'stage_aligment')
+def _get_ext_modules():
+  #build cython files
+  # python3 setup.py build_ext --inplace
+  path_parts = [module_name, 'analysis', 'ske_create', 'segWormPython', 'cython_files']
+  cython_path = os.path.join(*path_parts)
+  cython_path_e = os.path.join(module_name, 'analysis', 'stage_aligment')
 
-def _add_path(f_list):
-	return [os.path.join(cython_path, x) for x in f_list]
+  def _add_path(f_list):
+  	return [os.path.join(cython_path, x) for x in f_list]
 
-def _get_mod_path(name):
-    return '.'.join(path_parts + [name])
+  def _get_mod_path(name):
+      return '.'.join(path_parts + [name])
 
-ext_files = {
-	"circCurvature" : ["circCurvature.pyx", "c_circCurvature.c"],
-	"curvspace" : ["curvspace.pyx", "c_curvspace.c"]
-}
+  ext_files = {
+  	"circCurvature" : ["circCurvature.pyx", "c_circCurvature.c"],
+  	"curvspace" : ["curvspace.pyx", "c_curvspace.c"]
+  }
 
-include_dirs = [numpy.get_include()]
-ext_modules = cythonize(os.path.join(cython_path, "*_cython.pyx"))
-ext_modules += cythonize(os.path.join(cython_path_e, "*.pyx"))
-ext_modules += [Extension(_get_mod_path(name), 
-                          sources=_add_path(files), 
-                          include_dirs=include_dirs) 
-			   for name, files in ext_files.items()]
+  include_dirs = [numpy.get_include()]
+  ext_modules = cythonize(os.path.join(cython_path, "*_cython.pyx"))
+  ext_modules += cythonize(os.path.join(cython_path_e, "*.pyx"))
+  ext_modules += [Extension(_get_mod_path(name), 
+                            sources=_add_path(files), 
+                            include_dirs=include_dirs) 
+  			   for name, files in ext_files.items()]
+  return ext_modules
+
+
 
 #install setup
-setup(name=module_name,
-   version=__version__,
-   description='Tierpsy Tracker',
-   author='Avelino Javer',
-   author_email='avelino.javer@imperial.ac.uk',
-   url='https://github.com/ver228/tierpsy-tracker',
+setup(name=NAME,
+   version=VERSION,
+   description=DESCRIPTION,
+   author=AUTHOR,
+   author_email=AUTHOR_EMAIL,
+   url=URL,
    packages=['tierpsy'],
-
    cmdclass={'build_ext': build_ext},
-   ext_modules=ext_modules,
-   include_dirs=[numpy.get_include()]
+   ext_modules=_get_ext_modules(),
+   include_dirs=[numpy.get_include()],
+   include_package_data=True
    )
