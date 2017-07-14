3fc9cfbccf875c666c4fde3fd15a2e908b131adf
ver228 <ajaver@MRC-8791.local>
2017-07-14 17:08:40 +0100

    bug

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ from Cython.Distutils import build_ext
 import numpy
 import os
 
-NAME = "tierpsy"
+MODULE_NAME = "tierpsy"
 AUTHOR = 'Avelino Javer'
 AUTHOR_EMAIL = 'avelino.javer@imperial.ac.uk'
 URL = 'https://github.com/ver228/tierpsy-tracker'
 DOWNLOAD_URL = 'https://github.com/ver228/tierpsy-tracker'
 DESCRIPTION = "tierpsy: Tierpsy Tracker Multi-Worm Tracker."
-exec(open(module_name + '/version.py').read())
+exec(open(MODULE_NAME + '/version.py').read())
 VERSION = __version__
 
 def _get_ext_modules():
   #build cython files
   # python3 setup.py build_ext --inplace
-  path_parts = [module_name, 'analysis', 'ske_create', 'segWormPython', 'cython_files']
+  path_parts = [MODULE_NAME, 'analysis', 'ske_create', 'segWormPython', 'cython_files']
   cython_path = os.path.join(*path_parts)
-  cython_path_e = os.path.join(module_name, 'analysis', 'stage_aligment')
+  cython_path_e = os.path.join(MODULE_NAME, 'analysis', 'stage_aligment')
 
   def _add_path(f_list):
   	return [os.path.join(cython_path, x) for x in f_list]
@@ -46,7 +46,7 @@ def _get_ext_modules():
 
 
 #install setup
-setup(name=NAME,
+setup(name=MODULE_NAME,
    version=VERSION,
    description=DESCRIPTION,
    author=AUTHOR,
