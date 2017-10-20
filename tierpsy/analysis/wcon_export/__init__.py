d5911cb0efd21dc22ceccad6d5cdc90d80d1aa3d
ver228 <ajaver@MRC-8791.local>
2017-10-20 19:43:15 +0100

    .

--- tierpsy/analysis/wcon_export/__init__.py
+++ tierpsy/analysis/wcon_export/__init__.py
@@ -1,11 +1,25 @@
+from functools import partial
 from .exportWCON import exportWCON
 
+def _h_check_zip_integrity(fname):
+    try:
+        with zipfile.ZipFile(fname) as zf:
+            ret = zf.testzip()
+    except:
+        ret = -3
+
+    if ret is None:
+        return True
+    else:
+        return False
+#('is_valid_zip', partial(_h_check_zip_integrity, fn['wcon']))
+                        
 def args_(fn, param):
-	#arguments used by AnalysisPoints.py
-	return {
+    #arguments used by AnalysisPoints.py
+    return {
         'func': exportWCON,
         'argkws': {'features_file': fn['features']},
         'input_files' : [fn['features']],
         'output_files': [fn['features'], fn['wcon']],
-        'requirements' : ['FEAT_CREATE'],
+        'requirements' : ['FEAT_CREATE']
     }
