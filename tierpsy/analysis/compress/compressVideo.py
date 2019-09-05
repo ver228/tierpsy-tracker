b99d1dd2715c879a3e19cb9e8a4e4ec8ccd930d1
Luigi Feriani <luigi.feriani@gmail.com>
2019-09-05 17:50:29 +0100

    compressVideo.py: fixed bug that called the fov splitter class without an if si_fov_tosplit

--- tierpsy/analysis/compress/compressVideo.py
+++ tierpsy/analysis/compress/compressVideo.py
@@ -422,7 +422,8 @@ def compressVideo(video_file, masked_image_file, mask_param,  expected_fps=25,
         vid.release()
         
     # save fovsplitting data
-    fovsplitter.write_fov_wells_to_file(masked_image_file)
+    if is_fov_tosplit:
+        fovsplitter.write_fov_wells_to_file(masked_image_file)
 
     read_and_save_timestamp(masked_image_file)
     print_flush(base_name + ' Compressed video done.')
